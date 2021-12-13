995c08adea0b3bf7d692a3bf804f26827d61fa61
Noah <yuenuni@hotmail.com>
2021-12-13 11:19:12 +0800

    complete the project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,91 @@
+import React, { useEffect, useState } from 'react'
+import SignalCard from './components/SignalCard'
 import './App.css'
 
+const cardsImages = [
+  { "src": "/img/helmet-1.png", matched: false },
+  { "src": "/img/potion-1.png", matched: false },
+  { "src": "/img/ring-1.png", matched: false },
+  { "src": "/img/scroll-1.png", matched: false },
+  { "src": "/img/shield-1.png", matched: false },
+  { "src": "/img/sword-1.png", matched: false },
+]
+
 function App() {
+  const [cards, setCards] = useState([])
+  const [turn, setTurn] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+  const [disabled, setDisabled] = useState(false)
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardsImages, ...cardsImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setCards(shuffledCards)
+    setTurn(0)
+  }
+
+  // handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+  // reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setDisabled(false)
+  }
+
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true)
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) return { ...card, matched: true }
+            return card
+          })
+        })
+        resetTurn()
+      } else {
+        setTimeout(() => {
+          setTurn(prevTurn => prevTurn + 1)
+          resetTurn()
+        }, 1000)
+      }
+    }
+
+  }, [choiceOne, choiceTwo])
+
+  useEffect(() => {
+    shuffleCards()
+  }, [])
+
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map(card => (
+          <SignalCard
+            card={card}
+            key={card.id}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          />
+        ))}
+      </div>
+      <p>Turns: {turn}</p>
     </div>
   );
-} 
+}
 
 export default App
---
+++ src/components/SignalCard.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './SignalCard.css'
+
+function SignalCard({ card, handleChoice, flipped, disabled }) {
+
+	const handleClick = () => {
+		if(!disabled) handleChoice(card)
+	}
+
+	return (
+		<div className="card">
+			<div className={flipped ? "flipped" : ""}>
+				<img className="front" src={card.src} alt="card front" />
+				<img className="back" src="/img/cover.png" onClick={handleClick} alt="card back" />
+			</div>
+		</div>
+	);
+}
+
+export default SignalCard;
